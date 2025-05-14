710e4a908e37010eaff2bafed8890c6506f6a268
Ismael Silva <ismaelgomesesilva@gmail.com>
2025-05-13 21:33:20 -0300

    feat: Define routes for Project and Task resources with reorder functionality

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TaskController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('projects', ProjectController::class);
+Route::resource('tasks', TaskController::class);
+Route::post('tasks/reorder', [TaskController::class, 'reorder'])->name('tasks.reorder');
